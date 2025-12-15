bded86fadc4578fa126ba1c9b76e330db24c5ac3
Mark Derman <mark@soulv.co.za>
2025-12-15 14:14:03 +0200

    Date utility

---
+++ Miscellaneous/DateAndTime/DateOnlyExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Odin.DateAndTime
+{
+    /// <summary>
+    /// Extensions to DateOnly
+    /// </summary>
+    public static class DateOnlyExtensions
+    {
+        /// <summary>
+        /// Outputs the date in YYYYMMdd format as a System.Int64
+        /// </summary>
+        /// <param name="theDate"></param>
+        /// <returns></returns>
+        public static long ToLong(this DateOnly theDate)
+        {
+            return theDate.Year * 10000 + theDate.Month * 100 + theDate.Day;
+        }
+        
+        /// <summary>
+        /// Outputs the date in YYYYMMdd format as a System.Int32
+        /// </summary>
+        /// <param name="theDate"></param>
+        /// <returns></returns>
+        public static int ToInt32(this DateOnly theDate)
+        {
+            return theDate.Year * 10000 + theDate.Month * 100 + theDate.Day;
+        }
+    }
+}
